cd2468c92f2d4caedaf21c45a272313f034accce
MKP21 <meetknpatel2@gmail.com>
2020-09-24 21:09:17 -0400

    working user profile

--- app/care_api/views.py
+++ app/care_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+from care_api import serializers
+from care_api import models
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating a user"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
